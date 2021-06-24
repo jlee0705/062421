729d24e3e78c132602c222bc66dd4507d14444d9
kdypro11 <wns4315dl@naver.com>
2021-06-24 16:40:14 +0900

    today's work

---
+++ carFixMgr0624/carFixMgr0611/common/CommUtil.cs
@@ -0,0 +1,29 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace carFixMgr0611.common
+{
+    class CommUtil
+    {
+        public static void initTheme(Form form)
+        {
+            var mgr = MaterialSkinManager.Instance;
+            mgr.AddFormToManage(form as MaterialForm);
+            mgr.Theme =
+                MaterialSkinManager.Themes.LIGHT;
+            mgr.ColorScheme =
+                new ColorScheme(
+                    Primary.Indigo700,
+                    Primary.Indigo700,
+                    Primary.Indigo700,
+                    Accent.Indigo700,
+                    TextShade.WHITE);
+        }
+    }
+}
---
+++ carFixMgr0624/carFixMgr0611/ui/ReceiptView.cs
@@ -0,0 +1,50 @@
+﻿using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace carFixMgr0611.ui
+{
+    public partial class ReceiptView : MaterialForm
+    {
+        public ReceiptView()
+        {
+            InitializeComponent();
+        }
+
+        private void viewExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        public void initList()
+        {
+            string strPrice = string.Format("{0:#,0}원", 120000);
+            string[] data = { "1", "2021년6월24일", strPrice,
+                "김담당", "박고객"
+            };
+            viewList.Items.Add(new ListViewItem(data));
+
+            for (int i=0; i<50; i++)
+            {
+                viewList.Items.Add(new ListViewItem(
+                    new string[]
+                    {
+                        (i+2).ToString(),"2021년 6월24일","김담당","박고객"
+                    }
+                    ));
+            }
+        }
+
+        private void ReceiptView_Load(object sender, EventArgs e)
+        {
+            initList();
+        }
+    }
+}
